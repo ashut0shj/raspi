c67df6feeb95dde73bc6e3a47e5657ae1d66f1bf
ashut0shj <ashutoshj2810@gmail.com>
2023-04-09 17:33:35 +0530

    maza hi aa gaya mujhe to...

--- discoo.py
+++ discoo.py
@@ -4,91 +4,117 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #Delay time
 delay = 0.07
-
+ledl=[4,21,26,19,13,6]
 #Set the LED pins as output pins
-for x in range(2,10):
+for x in ledl:
     GPIO.setup(x,GPIO.OUT)
  
 #Pattern one
 def patternOne(times):
-    for a in range (0,times):
-        for i in range(2,10):
+    
+        for i in ledl:
            GPIO.output(i,GPIO.HIGH)
            sleep(delay)
            GPIO.output(i,GPIO.LOW)
 
 #Pattern two
 def patternTwo(times):
-    for a in range(0,times):
-        for i in range(2,10):
+    
+        for i in ledl:
             GPIO.output(i,GPIO.HIGH)
             sleep(delay)
             GPIO.output(i,GPIO.LOW)
             
-        for x in range(8,2,-1):
+        for xx in range(len(ledl)):
+            x=ledl[len(ledl)-xx-1]
             GPIO.output(x,GPIO.HIGH)
             sleep(delay)
             GPIO.output(x,GPIO.LOW)
 
 #Pattern three
 def patternThree(times):
-    for a in range(0,times):
-        for i in range(2,10):
+    
+        for i in ledl:
             GPIO.output(i,GPIO.LOW)
             sleep(delay)
             GPIO.output(i,GPIO.HIGH)
             
-        for x in range(8,2,-1):
+        for xx in range(len(ledl)):
+            x=ledl[len(ledl)-xx-1]
             GPIO.output(x,GPIO.LOW)
             sleep(delay)
             GPIO.output(x,GPIO.HIGH)
 
 #Pattern four
 def patternFour(times):
-    for a in range(0,times):
-        for i in range(2,10):
+    
+        for i in ledl:
             GPIO.output(i,GPIO.HIGH)
             sleep(delay)       
             
-        for x in range(9,2,-1):
+        for xx in range(len(ledl)):
+            x=ledl[len(ledl)-xx-1]
             GPIO.output(x,GPIO.LOW)
             sleep(delay)       
 
 #Pattern five
 def patternFive(times):
-    for a in range(0,times):
-        for i in range(2,9):
+    
+        for i in ledl:
             GPIO.output(i,GPIO.LOW)
             sleep(delay)       
             
-        for x in range(9,1,-1):
+        for xx in range(len(ledl)):
+            x=ledl[len(ledl)-xx-1]
             GPIO.output(x,GPIO.HIGH)
             sleep(delay)
 
 #Pattern six
 def patternSix(times):
-    for a in range(0,times):
-        for i in range(2,9):
+    
+        for i in ledl:
             GPIO.output(i,GPIO.HIGH)
             GPIO.output(i+1,GPIO.HIGH)
             sleep(delay)
             GPIO.output(i,GPIO.LOW)
             GPIO.output(i+1,GPIO.LOW)
             
-        for x in range(8,2,-1):
+        for xx in range(len(ledl)):
+            x=ledl[len(ledl)-xx-1]
             GPIO.output(x,GPIO.HIGH)
             GPIO.output(x-1,GPIO.HIGH)
             sleep(delay)
             GPIO.output(x,GPIO.LOW)
             GPIO.output(x-1,GPIO.LOW)
+
+c=0
+def runner(c):
+    if c<6:
+        c+=1
+        return c
+    else:
+        c=0
+        return c
     
+
+
 while True:
-    patternOne(3)
-    patternTwo(3)
-    patternThree(3)
-    patternFour(3)
-    patternFive(3)
-    patternSix(3)
+    GPIO.add_event_detect(16,GPIO.RISING,callback=runner)
+    
+    if c==0:
+        patternOne(3)
+    elif c==1:
+        patternTwo(3)
+    elif c==2:    
+        patternThree(3)
+    elif c==3:
+        patternFour(3)
+    elif c==4:
+        patternFive(3)
+    elif c==5:    
+        patternSix(3)
+
+
